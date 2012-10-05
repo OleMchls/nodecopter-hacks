affc3f7d5c532884d6b221ff396e88b6ebe1280c
Ole Michaelis <Ole.Michaelis@googlemail.com>
2012-10-05 14:39:38 +0200

    fixed imagestream blink

--- public/js/imagestream.js
+++ public/js/imagestream.js
@@ -1,8 +1,21 @@
 (function(){
 	var el = document.getElementById('screen')
 	var i = 0;
+	var img = document.createElement('img');
+	img.width = 1;
+	img.height = 1;
+	img.src = 'image.png'
+
+	img.onload = function() {
+		console.log(this.src)
+		el.style.backgroundImage = 'url('+this.src+')'
+	}
+
+	el.appendChild(img)
+	
+
 	setInterval(function() {
-		console.log(el.style.backgroundImage)
-		el.style.backgroundImage = ('url(/image.png?' + (i++) + ')')
+		img.src = ('/image.png?' + (i++))
+		//el.style.backgroundImage = ('url(/image.png?' + (i++) + ')')
 	}, 100)
 })()
